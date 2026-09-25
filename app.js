23b8d651a3a643074563a79b02467ec9568d9940
bradchips <rb.brinkman@gmail.com>
2026-09-25 08:21:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const SUPABASE_KEY = "sb_publishable_RB-K8vKRIk80fzUnO6bjaQ_bak6QV-e";
 const db = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 console.log("Supabase gekoppeld");
-const FAMILY_ID = "535e95f7-ef01-4cab-80e0-504aa2984";
+const FAMILY_ID = "535e95f7-ef01-4cab-80e0-504aa298475e";
 
 async function testDatabase() {
   const { data, error } = await db
